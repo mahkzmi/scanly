71da42cb8c828e89d33bbeb8ac94f6847aa03012
Mahdieh Kazemi <136577439+mahkzmi@users.noreply.github.com>
2025-08-12 14:24:37 +0330

    Update app.py
    change flask app to production

--- app.py
+++ app.py
@@ -151,5 +151,6 @@ def evaluate_score(results: dict) -> int:
 
     return min(score, 100)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
